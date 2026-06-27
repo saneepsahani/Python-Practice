8bc07765e4406fff0df11edc8f59d59c0df64014
saneepsahani <saneepsahanisk115@gmail.com>
2026-06-27 11:47:13 +0530

    Add Lesson 2 - Variables

---
+++ lesson2_variables.py
@@ -0,0 +1,13 @@
+name = "Saneep"
+age = 20
+city = "Deoria"
+college = "KIPM-CET"
+branch = "ECE"
+goal = "Embedded Systems Engineer"
+
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+print("College:", college)
+print("Branch:", branch)
+print("Goal:", goal)

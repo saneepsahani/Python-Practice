2e65cc523110ea32e30d40b2d71499f3758a56a7
saneepsahani <saneepsahanisk115@gmail.com>
2026-06-29 07:01:29 +0530

    Lesson 3 - User Input

---
+++ lesson3_user_input.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+college = input("Enter your college: ")
+branch = input("Enter your branch: ")
+
+print("\n------ Student Details ------")
+print("Name:", name)
+print("College:", college)
+print("Branch:", branch)
